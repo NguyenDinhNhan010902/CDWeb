438ae9af80d53b9262890a25855df38f066772cd
NguyenDinhNhan010902 <115056699+NguyenDinhNhan010902@users.noreply.github.com>
2024-05-03 18:43:06 +0700

    kết nối vs database

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -1,41 +1,67 @@
 import "./styleHome.css"
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
+import $ from 'jquery';
 import axios from 'axios';
-import logo from "./logo2.png";
-import { Link } from 'react-router-dom';
+import BASE_URL from '../../database/Config';
+import logo from "../../image/logo3.png";
+import {Link} from 'react-router-dom';
 
-function Home(){
+function Home() {
     const [data, setData] = useState([]);
     useEffect(() => {
-        axios.get('http://localhost:5000/detail')
-            .then(response => {
-                setData(response.data);
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
+        $.ajax({
+            url: `${BASE_URL}/detail`, // Sử dụng ký tự backtick và `${}` để sử dụng biến BASE_URL
+            type: 'GET',
+            success: function(response) {
+                setData(response);
+            },
+            error: function(error) {
+                console.error('Error fetching drtail: ', error);
+            }
+        });
     }, []);
     return <div className="container">
-        <div className="left">
-            {data.map(item => (
-                <div key={item.id}>
-                    <Link to="/path">
-                        <img src={item.img} alt="Logo" />
-                    </Link>
-                    <Link to="/path">
-                        <h3>{item.content}</h3>
-                    </Link>
-                </div>
-            ))}
+        <div className="home">
+            <div className="left">
+                {data.map(item => (
+                    <div key={item.id}>
+                        <Link to="/path">
+                            <img src={item.img} alt="Logo"/>
+                        </Link>
+                        <Link to="/path">
+                            <h3>{item.content}</h3>
+                        </Link>
+                    </div>
+                ))}
+            </div>
+            <div className="center">
+                {data.map(item => (
+                    <div key={item.id}>
+                        <Link to="/path">
+                            <img src={item.img} alt="Logo"/>
+                        </Link>
+                        <Link to="/path">
+                            <h3>{item.content}</h3>
+                        </Link>
+                    </div>
+                ))}
+            </div>
+            <div className="right">
+                {data.map(item => (
+                    <div key={item.id}>
+                        <Link to="/path">
+                            <img src={item.img} alt="Logo"/>
+                        </Link>
+                        <Link to="/path">
+                            <h3>{item.content}</h3>
+                        </Link>
+                    </div>
+                ))}
+            </div>
         </div>
-        <div className="center">
-            <Link  to="/path"><img src="./logo2.png"/></Link>
-            <Link to="/path"><h3>Tiêu Đề</h3> </Link>
-        </div>
-        <div className="right">
-            <Link  to="/path"><img src={logo}/></Link>
-            <Link to="/path"><h3>Tiêu Đề</h3> </Link>
-        </div>
+        <div className="khuvuc"></div>
+
     </div>
 }
+
 export default Home
